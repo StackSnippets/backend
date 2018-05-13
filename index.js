8fa6cdcaaa2e1bffc4c3f64ee07443c067336910
BoyWithSilverWings <agney.menons@live.com>
2018-05-13 14:30:52 +0530

    intial setup

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+import express from "express";
+import compression from "compression";
+import helmet from "helmet";
+import session from "express-session";
+import { json } from "body-parser";
+import { load } from "dotenv";
+import path from "path";
+import logger from "morgan";
+import cors from "cors";
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+load({ path: '.env' });
+
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.use(logger('dev'));
+app.use(compression());
+app.use(helmet());
+
+app.use(json());
+app.use(cors());
+
+app.listen(app.get('port'), () => {
+  console.log(`App running on port ${app.get('port')}`);
+});
