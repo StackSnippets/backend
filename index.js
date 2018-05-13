59a9211e99147668dda636c30eff913746de3348
BoyWithSilverWings <agney.menons@live.com>
2018-05-13 14:55:11 +0530

    prettier setup

--- index.js
+++ index.js
@@ -8,18 +8,18 @@ import path from "path";
 import logger from "morgan";
 import cors from "cors";
 
-load({ path: '.env' });
+load({ path: ".env" });
 
 const app = express();
 
-app.set('port', process.env.PORT || 3000);
-app.use(logger('dev'));
+app.set("port", process.env.PORT || 3000);
+app.use(logger("dev"));
 app.use(compression());
 app.use(helmet());
 
 app.use(json());
 app.use(cors());
 
-app.listen(app.get('port'), () => {
-  console.log(`App running on port ${app.get('port')}`);
+app.listen(app.get("port"), () => {
+  console.log(`App running on port ${app.get("port")}`);
 });
