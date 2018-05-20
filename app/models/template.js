a54c766630ca51b98cf2b384eb3cd65bcaa4d06d
BoyWithSilverWings <agney.menons@live.com>
2018-05-20 20:32:17 +0530

    set defaults for base and favorited

--- app/models/template.js
+++ app/models/template.js
@@ -5,8 +5,8 @@ const Schema = mongoose.Schema;
 const Template = new Schema({
   name: { type: String, required: true, max: 100 },
   code: { type: String, required: true },
-  base: Boolean,
-  favorited_by: Number,
+  base: { type: Boolean, default: false },
+  favorited_by: { type: Number, default: 0 },
   description: String,
   created: { type: Date, default: Date.now }
 });
