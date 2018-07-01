af4148500eebc0558487f1acd4ffdcec50c97b61
BoyWithSilverWings <agney.menons@live.com>
2018-07-01 20:23:57 +0530

    connect to mlab

--- connection.js
+++ connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 class Database {
   static connect() {
-    const url = "mongodb://127.0.0.1/snippets";
+    const url = process.env.MONGO_URL;
     mongoose.connect(url);
     const db = mongoose.connection;
     db.on("error", console.error.bind(console, "MongoDB connection error:"));
