bc12b5bf8d0d8f5138bc2ff3d9e0a6ddf7bb5d71
BoyWithSilverWings <agney@outlook.in>
2018-12-09 19:54:30 +0530

    add timestamps to template syntax

--- app/models/template.js
+++ app/models/template.js
@@ -2,13 +2,17 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const Template = new Schema({
-  name: { type: String, required: true, max: 100 },
-  code: { type: String, required: true },
-  base: { type: Boolean, default: false },
-  favorited_by: { type: Number, default: 0 },
-  description: String,
-  created: { type: Date, default: Date.now }
-});
+const Template = new Schema(
+  {
+    name: { type: String, required: true, max: 100 },
+    code: { type: String, required: true },
+    base: { type: Boolean, default: false },
+    favorited_by: { type: Number, default: 0 },
+    description: String,
+  },
+  {
+    timestamps: {},
+  }
+);
 
 module.exports = mongoose.model("Template", Template);
