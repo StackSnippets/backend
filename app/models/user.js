e24d1c56c8b0f8c585735af029289460f34e0a68
BoyWithSilverWings <agney@outlook.in>
2018-12-17 23:02:50 +0530

    add github ID and email to user info

--- app/models/user.js
+++ app/models/user.js
@@ -2,9 +2,15 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const User = new Schema({
-  name: { type: String, required: true },
-  id: { type: String, required: true },
-});
+const User = new Schema(
+  {
+    name: { type: String, required: true },
+    githubId: { type: String, required: true },
+    email: { type: String, required: true },
+  },
+  {
+    timestamps: {},
+  }
+);
 
 module.exports = User;
